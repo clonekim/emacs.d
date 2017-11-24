f5eac21aaf76aec3d31bf5d3ab4d50884e0b0047
DAEHEE KIM <daehee.kim@oho.co.kr>
2017-11-24 16:22:57 +0900

    저장소추가

--- init.el
+++ init.el
@@ -1,8 +1,9 @@
 (when (>= emacs-major-version 24)
   (require 'package)
-  (add-to-list 
-   'package-archives '("melpa" . "http://melpa.org/packages/")
-   t)
+  (setq package-archives '(("melpa" . "http://melpa.org/packages/") 
+                           ("gnu" . "http://elpa.gnu.org/packages/")
+                           ("org" . "http://orgmode.org/elpa/")))
+
   (package-initialize))
 
 (add-to-list 'load-path "~/.emacs.d/customizations")
