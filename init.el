ad81abc396563ef717a8fa8b3141cada62a47431
daehee.kim <daehee.kim@oho.co.kr>
2018-06-14 21:16:21 +0900

    불필요한 설정생성 방지

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
 (add-to-list 'load-path "~/.emacs.d/customizations")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+(setq package-enable-at-startup nil) ;;improves startup time
 
 (load "shell-integration.el")
 (load "navigation.el")
@@ -25,47 +26,8 @@
 ;; (load "neo.el")
 (load "git.el")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(compilation-message-face (quote default))
- '(custom-enabled-themes (quote (wombat)))
- '(fci-rule-color "#37474f")
- '(hl-sexp-background-color "#1c1f26")
- '(linum-format "%5d")
- '(magit-diff-section-arguments (quote ("--no-ext-diff")))
- '(package-selected-packages
-   (quote
-    (magit web-mode tagedit rainbow-delimiters popwin paredit js2-mode ido-completing-read+ helm-smex go-eldoc go-autocomplete flymake-go flycheck exec-path-from-shell emmet-mode direx clojure-mode-extra-font-locking cider)))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#f36c60")
-     (40 . "#ff9800")
-     (60 . "#fff59d")
-     (80 . "#8bc34a")
-     (100 . "#81d4fa")
-     (120 . "#4dd0e1")
-     (140 . "#b39ddb")
-     (160 . "#f36c60")
-     (180 . "#ff9800")
-     (200 . "#fff59d")
-     (220 . "#8bc34a")
-     (240 . "#81d4fa")
-     (260 . "#4dd0e1")
-     (280 . "#b39ddb")
-     (300 . "#f36c60")
-     (320 . "#ff9800")
-     (340 . "#fff59d")
-     (360 . "#8bc34a"))))
- '(vc-annotate-very-old-color nil))
+(setq custom-file (make-temp-file "emacs-custom"))
+(load-theme 'aanila t)
 
-;;; for wombat
-(set-face-attribute hl-line-face nil :underline nil)
-(set-face-background 'hl-line "grey27")
+(set-face-background 'mode-line "black")
+(set-face-foreground 'mode-line "yellow")
