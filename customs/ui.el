285eeaf59e462b0fbdab9d6b5c6dc3962d13b8f3
DAEHEE KIM <clonekim@gmail.com>
2021-02-08 20:16:18 +0900

    Update ui.el

--- customs/ui.el
+++ customs/ui.el
@@ -3,16 +3,25 @@
 ;;----------------------------------------------------------------------------
 
 (when (display-graphic-p)
+  (menu-bar-mode -1)
   (column-number-mode t)
   (size-indication-mode t)
   (setq use-dialog-box nil)
   (setq use-file-dialog nil)
-  (setq scroll-margin 0
+  (setq 
+      scroll-margin 0
       scroll-conservatively 100000
       scroll-preserve-screen-position 1)
   (setq inhibit-startup-screen t)
   (setq inhibit-splash-screen t)
   (setq initial-frame-alist '((width . 120) (height . 60))))
+
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+;; Don't show native OS scroll bars for buffers because they're redundant
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
 
 (when (display-graphic-p)
    (set-face-attribute 'default nil :font (if (eq system-type 'darwin) "Andale Mono 12" "DejaVu Sans Mono 9"))
