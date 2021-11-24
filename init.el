229fa29ec79780e1c5102b301dbc9c1e2694c692
DAEHEE KIM <clonekim@gmail.com>
2021-11-24 12:43:54 +0900

    Update init.el

--- init.el
+++ init.el
@@ -1,12 +1,12 @@
 (require 'package)
+
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 (setq package-enable-at-startup nil)
 (setq package-archive-priorities '(("melpa-stable" . 1)))
-(package-initialize)
 
-(unless package-archive-contents
-  (package-refresh-contents))
+(package-initialize)
 
 ;; use-package 가 설치되어 있지 않으면 설치
 (unless (package-installed-p 'use-package)
@@ -14,31 +14,4 @@
   (package-install 'use-package)
   (eval-when-compile (require 'use-package)))
 
-(setq use-package-always-ensure t)
-
-(add-to-list 'load-path "~/.emacs.d/dotfiles")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-
-;;가비지콜렉터 쓰레드홀드
-(setq gc-cons-threshold 10000000)
-
-;; Restore after startup
-(add-hook 'after-init-hook
-          (lambda ()
-            (setq gc-cons-threshold 1000000)
-            (message "gc-cons-threshold restored "
-                     gc-cons-threshold)))
-
-
-
-(load "bootstrap.el")
-(load "use-company.el")
-(load "use-evil.el")
-(load "use-helm.el")
-(load "use-paredit.el")
-(load "use-git.el")
-(load "use-lsp.el")
-(load "doom.el")
-(load "web.el")
-(load "theme.el")
-(load "use-dashboard.el")
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
